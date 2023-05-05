b789a88fe9e38ff3cdc54b5d386c50aea7bff8eb
Deepakkumar-4 <godotdownload@gmail.com>
2023-05-06 01:47:46 +0530

    Project started Name: Tripity Stage one creating login activity

--- app/src/main/java/com/example/tripity/Activities/MainActivity.java
+++ app/src/main/java/com/example/tripity/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.tripity;
+package com.example.tripity.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.tripity.R;
 
 public class MainActivity extends AppCompatActivity {
 
