353ad9211da6959893246fc21ccf502a07a1e100
Deepakkumar-4 <godotdownload@gmail.com>
2023-05-08 02:25:46 +0530

    Project started Name: Tripity login Activity  working otp receiving and verifing successfully dashboard activity added and layout of profilesetup is done

--- app/src/main/java/com/example/tripity/ProfileSetupActivity.java
+++ app/src/main/java/com/example/tripity/ProfileSetupActivity.java
@@ -2,13 +2,30 @@ package com.example.tripity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileSetupActivity extends AppCompatActivity {
+
+    Button save;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_setup);
+
+        save =findViewById(R.id.saveInfo);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(ProfileSetupActivity.this,DashboardActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
