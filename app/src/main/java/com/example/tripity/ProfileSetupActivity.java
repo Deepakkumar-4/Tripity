b7ebda112aeb978a8ded42bfcaa033cbf3fd0bf9
Deepakkumar-4 <godotdownload@gmail.com>
2023-05-10 01:28:40 +0530

    Project started Name: Tripity -refined layout for profile activity -added bottom nav bar on Dashboard with fragments and reduces time of splash screen -login Activity  working -otp receiving and verifing successfully -dashboard activity added -layout of profilesetup is done -Splash Screeen Added wth jump to dashBoard if already logedin

--- app/src/main/java/com/example/tripity/ProfileSetupActivity.java
+++ app/src/main/java/com/example/tripity/ProfileSetupActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
-public class ProfileSetupActivity extends AppCompatActivity {
+public class ProfileSetupActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Button save;
 
@@ -17,15 +20,32 @@ public class ProfileSetupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_setup);
 
-        save =findViewById(R.id.saveInfo);
+        Spinner spinner = findViewById(R.id.identityType);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter
+                .createFromResource(this, R.array.identityProof, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        save = findViewById(R.id.saveInfo);
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(ProfileSetupActivity.this,DashboardActivity.class);
+                Intent intent = new Intent(ProfileSetupActivity.this, DashboardActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+        String selected = adapterView.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
