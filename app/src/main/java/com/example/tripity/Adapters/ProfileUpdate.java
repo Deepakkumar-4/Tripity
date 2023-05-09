6b1459fb6a338794fa46a76fe5839491c648d275
Deepakkumar-4 <godotdownload@gmail.com>
2023-05-10 02:33:01 +0530

    Project started Name: Tripity -taking data from profilesetup and uploading it as new user everytime -refined layout for profile activity -added bottom nav bar on Dashboard with fragments and reduces time of splash screen -login Activity  working -otp receiving and verifing successfully -dashboard activity added -layout of profilesetup is done -Splash Screeen Added wth jump to dashBoard if already logedin

---
+++ app/src/main/java/com/example/tripity/Adapters/ProfileUpdate.java
@@ -0,0 +1,38 @@
+package com.example.tripity.Adapters;
+
+public class ProfileUpdate {
+    String name,email,Age,idtype,idnum,nationlity;
+
+    public ProfileUpdate(String name, String email, String age, String idtype, String idnum, String nationlity) {
+        this.name = name;
+        this.email = email;
+        this.Age = age;
+        this.idtype = idtype;
+        this.idnum = idnum;
+        this.nationlity = nationlity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public String getIdtype() {
+        return idtype;
+    }
+
+    public String getIdnum() {
+        return idnum;
+    }
+
+    public String getNationality() {
+        return nationlity;
+    }
+}
--- app/src/main/java/com/example/tripity/ProfileSetupActivity.java
+++ app/src/main/java/com/example/tripity/ProfileSetupActivity.java
@@ -8,19 +8,35 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.tripity.Adapters.ProfileUpdate;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileSetupActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    EditText uname,emailAdd,age,nationality,identityNum;
+    Spinner spinner;
     Button save;
-
+    FirebaseDatabase database;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_setup);
+        uname =findViewById(R.id.username);
+        emailAdd =findViewById(R.id.emailAdd);
+        age =findViewById(R.id.Age);
+        nationality = findViewById(R.id.nationality);
+        identityNum = findViewById(R.id.idNumber);
 
-        Spinner spinner = findViewById(R.id.identityType);
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("UserPrimaryKey");
+
+        spinner = findViewById(R.id.identityType);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter
                 .createFromResource(this, R.array.identityProof, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -31,6 +47,7 @@ public class ProfileSetupActivity extends AppCompatActivity implements AdapterVi
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                insertProfileData();
                 Intent intent = new Intent(ProfileSetupActivity.this, DashboardActivity.class);
                 startActivity(intent);
                 finish();
@@ -39,9 +56,27 @@ public class ProfileSetupActivity extends AppCompatActivity implements AdapterVi
 
     }
 
+    //insert data to data dabe
+    private  void insertProfileData(){
+
+        String name =uname.getText().toString();
+        String Age = age.getText().toString();
+        String email= emailAdd.getText().toString();
+        String idProof =spinner.getSelectedItem().toString();
+        String identity = identityNum.getText().toString();
+        String nationlityType = nationality.getText().toString();
+
+
+        ProfileUpdate profileUpdate =new ProfileUpdate( name,  email,  Age,  idProof,  identity,  nationlityType);
+        databaseReference.push().setValue(profileUpdate);
+        Toast.makeText(this, "Updated", Toast.LENGTH_SHORT).show();
+        
+    }
+
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
         String selected = adapterView.getItemAtPosition(position).toString();
+    
     }
 
     @Override
